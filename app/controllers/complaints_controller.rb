044688e13a3ae2f939ba66f5d7e40fdad73997b4
Kenan Warren <redorb1790@gmail.com>
2016-02-20 14:22:21 -0500

    Refactor to more generalize group queries and reduce routes

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,74 +1,65 @@
 class ComplaintsController < ApplicationController
+  VALID_GROUPING_COLS = [ 'case_summary',
+                          'agency',
+                          'division',
+                          'type',
+                          'topic',
+                          'council_district',
+                          'police_district',
+                          'major_area',
+                          'neighborhood' ]
+
   def full_count_by_date
     @complaints = Complaint.where("case_created IS NOT NULL").group("SUBSTR(case_created,1,10)").count
     render json: @complaints
   end
 
-  def count_by_date_group_on_summary
-    render_complaints_with_group 'case_summary', params[:with_group_date]
+  def count_by_groups
+    begin
+      @complaints = Complaint
+      build_group_query
+      @complaints = @complaints.count
+      render json: @complaints
+    rescue => error
+      render json: {error: error.message}
+    end
+
   end
 
-  def count_by_date_group_on_agency
-    render_complaints_with_group 'agency', params[:with_group_date]
-  end
-
-  def count_by_date_group_division
-    render_complaints_with_group 'division', params[:with_group_date]
-  end
-
-  def count_by_date_group_type
-    render_complaints_with_group 'type', params[:with_group_date]
-  end
-
-  def count_by_date_group_topic
-    render_complaints_with_group 'topic', params[:with_group_date]
-  end
-
-  def count_by_date_group_council_dist
-    render_complaints_with_group 'council_district', params[:with_group_date]
-  end
-
-  def count_by_date_group_police_dist
-    render_complaints_with_group 'police_district', params[:with_group_date]
-  end
-
-  def count_by_date_group_area
-    render_complaints_with_group 'major_area', params[:with_group_date]
-  end
-
-  def count_by_date_group_neighborhood
-    render_complaints_with_group 'neighborhood', params[:with_group_date]
+  def count_by_date_and_groups
+    begin
+      @complaints = Complaint.where("case_created IS NOT NULL").group("SUBSTR(case_created,1,10)")
+      build_group_query_with_count
+      render json: @complaints
+    rescue => error
+      render json: {error: error.message}
+    end
   end
 
   def complaints_by_area_from_coords
-    @complaints = Complaint.within(0.1, :origin => [39.691552,-104.978432]).count
+    @complaints = Complaint.where("case_summary IS NOT NULL").group('case_summary').within(0.2, :origin => [39.691552,-104.978432]).count
+    render json: @complaints
+  end
+
+  def info_by_groups
+    build_group_query
     render json: @complaints
   end
 
   private
-    def render_complaints_with_group group, with_group_date
-      begin
-        with_group_date = with_group_date.present? ? with_group_date.to_bool : true
-        if with_group_date
-          @complaints = count_by_date_group_on_column group
-        else
-          @complaints = count_group group
+    def build_group_query
+      params[:groups].each do |group|
+        group = group.downcase
+        unless VALID_GROUPING_COLS.include?(group)
+          raise "#{group} is not a valid group, choose from #{VALID_GROUPING_COLS}"
         end
 
-        render json: @complaints
-      rescue
-        render json: {error: 'invalid param, only bool values are accepted'}
+        @complaints = @complaints.where("#{group} IS NOT NULL").group(group)
       end
     end
-
-    def count_by_date_group_on_column column_name
-      Complaint.where("case_created IS NOT NULL AND #{column_name} IS NOT NULL")
-          .group(column_name).group("SUBSTR(case_created,1,10)").count
-    end
-
-    def count_group column_name
-      Complaint.where("#{column_name} IS NOT NULL")
-          .group(column_name).count
+    def build_group_query_with_count
+      build_group_query
+      @complaints = @complaints.count
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   get 'full_count_by_date', to: 'complaints#full_count_by_date'
-  get 'count_by_date_group_on_summary(/:with_group_date)', to: 'complaints#count_by_date_group_on_summary'
-  get 'count_by_date_group_on_agency(/:with_group_date)', to: 'complaints#count_by_date_group_on_agency'
-  get 'count_by_date_group_division(/:with_group_date)', to: 'complaints#count_by_date_group_division'
-  get 'count_by_date_group_type(/:with_group_date)', to: 'complaints#count_by_date_group_type'
-  get 'count_by_date_group_topic(/:with_group_date)', to: 'complaints#count_by_date_group_topic'
-  get 'count_by_date_group_council_dist(/:with_group_date)', to: 'complaints#count_by_date_group_council_dist'
-  get 'count_by_date_group_police_dist(/:with_group_date)', to: 'complaints#count_by_date_group_police_dist'
-  get 'count_by_date_group_area(/:with_group_date)', to: 'complaints#count_by_date_group_area'
-  get 'count_by_date_group_neighborhood(/:with_group_date)', to: 'complaints#count_by_date_group_neighborhood'
   get 'complaints_by_area_from_coords', to: 'complaints#complaints_by_area_from_coords'
+  get 'count_by_groups(/:groups)', to: 'complaints#count_by_groups'
+  get 'count_by_date_and_groups(/:groups)', to: 'complaints#count_by_date_and_groups'
+  get 'info_by_groups(/:groups)', to: 'complaints#info_by_groups'
 end
